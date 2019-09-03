7f8b9413d38eb55cfb827df81888f371c4c6e1dc
Guilherme Costa <guilherme.costa@totvs.com.br>
2019-09-03 11:52:29 -0300

    implementing simple backend

--- node-mongo-compose/backend/app.js
+++ node-mongo-compose/backend/app.js
@@ -2,12 +2,31 @@ const express = require('express');
 const restful = require('node-restful');
 const server = express();
 const mongoose = restful.mongoose;
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 
 // Database
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://db/mydb');
 
-server.get('/', (req, res, next) => {
-    res.send('backend');
+// Middlewares
+server.use(bodyParser.urlencoded({extended: true}));
+server.use(bodyParser.json());
+server.use(cors());
+
+// ODM
+const Client = restful.model('Client', {
+    name: { type: String, required: true }
 });
+
+// Rest API
+Client.methods(['get', 'post', 'put', 'delete']);
+Client.updateOptions({new: true, runValidators: true});
+
+// Routes
+Client.register(server, '/clients');
+
+// Server start
+server.listen(3000);
+console.log('listening server at port 3000');
